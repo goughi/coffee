5876ca9c51f6a87f948baee562c5b164fb41e998
goughi <goughi@hotmail.com>
2015-11-27 13:06:50 +0000

    newCommit

---
+++ coffeeTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="coffeeTest.MvcApplication" Language="C#" %>
---
+++ coffeeTest/Models/CoffeeStore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace coffeeTest.Models
+{
+    public class CoffeeStore
+    {
+        public string [] coffees { get; set; }
+        public int Rating { get; set; }
+    }
+}
